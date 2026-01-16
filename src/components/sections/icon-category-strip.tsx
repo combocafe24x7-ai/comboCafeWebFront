cee45984c1ce46eaf036c7e63fbd63a87f0e2ae1
Jishu Das <jishudas329@gmail.com>
2026-01-16 12:28:58 +0000

    keep very little space betwwen them

--- src/components/sections/icon-category-strip.tsx
+++ src/components/sections/icon-category-strip.tsx
@@ -7,9 +7,9 @@ export default function IconCategoryStrip() {
   return (
     <section className="bg-white py-8">
       <div className="container mx-auto">
-        <div className="flex items-start md:grid md:grid-cols-4 md:gap-4 md:justify-around text-center gap-2 overflow-x-auto scrollbar-hide">
+        <div className="flex items-start md:grid md:grid-cols-4 md:gap-2 md:justify-around text-center gap-2 overflow-x-auto scrollbar-hide">
           {config.iconCategories.map((category: any) => (
-            <Link href={category.href} key={category.id} className="group shrink-0 w-24">
+            <Link href={category.href} key={category.id} className="group shrink-0 w-20">
               <div className="flex justify-center items-center">
                   <div className="relative w-20 h-20 rounded-lg bg-accent flex items-center justify-center group-hover:bg-primary/20 transition-colors duration-300 overflow-hidden">
                       {category.imageUrl ? (
@@ -19,7 +19,7 @@ export default function IconCategoryStrip() {
                       )}
                   </div>
               </div>
-              <h4 className="mt-4 font-semibold text-sm text-gray-700 group-hover:text-primary transition-colors duration-300">{category.label}</h4>
+              <h4 className="mt-2 font-semibold text-sm text-gray-700 group-hover:text-primary transition-colors duration-300">{category.label}</h4>
             </Link>
           ))}
         </div>
