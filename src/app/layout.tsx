9f712aed9ef406550d1fdc80e53fa326ce79ac4e
Jishu Das <jishudas329@gmail.com>
2026-01-16 12:54:17 +0000

    can you remove the extra space betweeen this and  the navigation bar (_f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         >
           <CartProvider>
               <Header />
-              <div className='md:pt-0 pt-28'>
+              <div className='md:pt-0 pt-24'>
                 {children}
               </div>
               <MobileBottomNav />
--- src/components/mobile/FreeDeliveryPill.tsx
+++ src/components/mobile/FreeDeliveryPill.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function FreeDeliveryPill() {
   return (
-    <div className="px-4 py-2">
+    <div className="px-4">
       <div className="relative aspect-[350/65] w-full overflow-hidden rounded-lg">
         <Image
           src="https://gpfocwgfedokhmfsbcpy.supabase.co/storage/v1/object/public/asset/image%20(3).png"
